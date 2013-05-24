f724d179eefa88c24208f6eb44ebd96e971155e1
drammopo <drammopo@gmail.com>
2013-05-23 22:49:24 -0500

    Recruiter belongs_to and has_many relationships

--- app/models/recruiter.rb
+++ app/models/recruiter.rb
@@ -1,5 +1,9 @@
 class Recruiter < ActiveRecord::Base
-  belongs_to :company
+
+  has_many :note_recruiters
+  has_many :candidate_recruiters
+  has_many :job_recruiters
+  has_many :agency_recruiters
 
   def full_name
     return "#{first_name} #{last_name}"
