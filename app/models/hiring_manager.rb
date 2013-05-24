a0c628b5ec7f4c6750bf967ed42532720857ffdd
drammopo <drammopo@gmail.com>
2013-05-23 22:51:28 -0500

    Hiring Manager belongs_to and has_many relationships

--- app/models/hiring_manager.rb
+++ app/models/hiring_manager.rb
@@ -1,6 +1,8 @@
 class HiringManager < ActiveRecord::Base
-  belongs_to :company
+
+  belongs_to :client
   has_many :hiring_manager_notes
+  has_many :jobs
 
   def full_name
     return "#{first_name} #{last_name}"
